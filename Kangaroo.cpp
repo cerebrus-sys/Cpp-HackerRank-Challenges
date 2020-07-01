18e71e4e7e08f47d6248db1d0d688eda620ac2e8
Sanjan <sanjangeet2109s@gmail.com>
2020-07-01 10:04:18 +0530

    Solved Kangaroo

---
+++ Kangaroo.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<string> split_string(string);
+
+string kangaroo(int x1, int v1, int x2, int v2) {
+    int x = x1 - x2;
+    int v = v2 - v1;
+
+    if (v == 0)
+        return "NO";
+
+    return ((x % v == 0) && (x / v >= 0)) ? "YES" : "NO";
+}
+
+int main()
+{
+    string x1V1X2V2_temp;
+    getline(cin, x1V1X2V2_temp);
+
+    vector<string> x1V1X2V2 = split_string(x1V1X2V2_temp);
+
+    int x1 = stoi(x1V1X2V2[0]),
+        v1 = stoi(x1V1X2V2[1]),
+        x2 = stoi(x1V1X2V2[2]),
+        v2 = stoi(x1V1X2V2[3]);
+
+    cout << kangaroo(x1, v1, x2, v2) << "\n";
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
