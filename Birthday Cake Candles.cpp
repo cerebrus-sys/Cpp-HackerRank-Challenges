eed9753394e69aa2312ceb62e1bd422604bc33dd
Sanjan <sanjangeet2109s@gmail.com>
2020-07-01 09:58:57 +0530

    Solved Birthday Cake Candles

---
+++ Birthday Cake Candles.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<string> split_string(string);
+
+int max(vector<int> ar) {
+    int m = ar[0];
+    for (auto i: ar)
+        if (m < i)
+            m = i;
+    return m;
+}
+
+int birthdayCakeCandles(vector<int> ar) {
+    int m = max(ar);
+    int r = 0;
+    for (auto i: ar)
+        if (m == i)
+            r++;
+    return r;
+}
+
+int main()
+{
+    int ar_count;
+    cin >> ar_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string ar_temp_temp;
+    getline(cin, ar_temp_temp);
+
+    vector<string> ar_temp = split_string(ar_temp_temp);
+
+    vector<int> ar(ar_count);
+
+    for (int i = 0; i < ar_count; i++) {
+        int ar_item = stoi(ar_temp[i]);
+
+        ar[i] = ar_item;
+    }
+
+    int result = birthdayCakeCandles(ar);
+
+    cout << result << "\n";
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
